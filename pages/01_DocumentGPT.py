c4163cc1fc309f0412c712524d259e6292802f5f
Hoi Won KIM <gw31513@gmail.com>
2023-12-06 12:47:03 +1300

    DOCUMENTGPT_Streaming

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -7,6 +7,7 @@ from langchain.storage import LocalFileStore
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.base import BaseCallbackHandler
 import streamlit as st
 
 st.set_page_config(
@@ -14,8 +15,25 @@ st.set_page_config(
     page_icon="📃",
 )
 
+class ChatCallbackHandler(BaseCallbackHandler):
+    message = ""
+
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+
 llm = ChatOpenAI(
     temperature=0.1,
+    streaming=True,
+    callbacks=[
+        ChatCallbackHandler(),
+    ],
 )
 
 # st.title("DocumentGPT")
@@ -56,11 +74,14 @@ def embed_file(file):
     retriever = vectorstore.as_retriever()
     return retriever
 
+def save_message(message, role):
+    st.session_state["messages"].append({"message": message, "role": role})
+
 def send_message(message, role, save=True):
     with st.chat_message(role):
         st.markdown(message)
     if save:
-        st.session_state["messages"].append({"message": message, "role": role})
+        save_message(message, role)
 
 
 def paint_history():
@@ -129,8 +150,8 @@ if file:
             | prompt
             | llm
         )
-        response = chain.invoke(message)
-        send_message(response.content, "ai")
-        
+        with st.chat_message("ai"):
+            response = chain.invoke(message)
+
 else:
     st.session_state["messages"] = []
